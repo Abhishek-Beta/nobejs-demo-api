241e8de062bf1b164c610befd0dd79b547b96e59
Abhishek-Potula <abhishek@betalectic.com>
2024-02-06 15:56:42 +0530

    Table name added

--- database/migrations/20240205120938_create_users_table.js
+++ database/migrations/20240205120938_create_users_table.js
@@ -1,16 +1,12 @@
 exports.up = async function (knex) {
     await knex.raw(`create extension if not exists "uuid-ossp"`);
-    return knex.schema.createTable("users", function (table) {
-      table
-        .uuid("uuid")
-        .notNullable()
-        .primary()
-        .defaultTo(knex.raw("uuid_generate_v4()"));
-      table.string("email", 255).notNullable();
-      table.string("password", 255).notNullable();
+    return knex.schema.createTable("items", function (table) {
+      
+      table.increments('id')
+      table.string("name", 255).notNullable();
     });
   };
   
 exports.down = function (knex) {
-    return knex.schema.dropTable("teams");
+    return knex.schema.dropTable("items");
 };
